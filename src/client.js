5772d9d92ced90c274290d74e828f6dca0a00145
chrisrdaniels <chrisdaniels765@gmail.com>
2021-12-13 19:15:49 -0700

    recommit to new github repository

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import * as contentful from "contentful";
+
+export const client = contentful.createClient({
+	space: "da51ehyz1oaf",
+	accessToken: "5HssTwT6rj4EubAM0e6UV6jWwC-S5pSq1hDozjsa9jI"
+});
